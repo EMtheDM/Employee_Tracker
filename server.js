08d91f399932ec086b5f0d88aa442909524cd04d
Eric Martin <eric_martin@me.com>
2021-11-16 16:27:39 -0700

    adding questions to inquirer

--- server.js
+++ server.js
@@ -13,3 +13,47 @@ db.connect((error) => {
     questions();
 });
 
+const questions = () => {
+    inquirer
+        .prompt([
+            {
+                name: "choices",
+                type: "list",
+                message: "What would you like to do?",
+                choices: [
+                    "View all departments",
+                    "View all roles",
+                    "View all employees",
+                    "Add a deparment",
+                    "Add a role",
+                    "Add an employee",
+                    "Update an employee role"
+                ],
+            },
+        ])
+        .then((answers) => {
+            const { choices } = answers;
+            if (choices == "View all departments") {
+                loadDepts();
+            } else if (choices == "View all roles") {
+                loadRoles();
+            } else if (choices == "View all employees") {
+                loadEmployees();
+            } else if (choices == "Add a department") {
+                addDept();
+            } else if (choices == "Add a role") {
+                addRole();
+            } else if (choices == "Add an employee") {
+                addEmployee();
+            } else {
+                updateEmployeeRole();
+            };
+        });
+};
+
+const loadDepts = () => {
+    db.query("SELECT * FROM department", function (err, result) {
+        console.table(result);
+        questions();
+    });
+};
