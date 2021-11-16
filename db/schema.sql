e169ca5702e7807da518e08a7ae6793a8ff0a436
Eric Martin <eric_martin@me.com>
2021-11-16 15:29:57 -0700

    added schema.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,25 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    manager_id INT,
+    FOREING KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
